2084b5cf9bfc6eda27b193d3924ad35ea157a046
Gavin <waltersg298@gmail.com>
2023-08-01 20:06:35 -0600

    working on filepaths

---
+++ filesystems/filepaths.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3 
+
+class FilePaths:
+    def __init__():
+        pass
---
+++ tests/test_filesystems.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from filesystems.filepaths import FilePaths
+
+def test_initial():
+    assert True
+
